4bdce461c638e0e48c56b06bbad31268ec2855a0
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-21 14:50:41 +0100

    com16 zad1 WPF klasa HistoryItem w pliku cs WPF'a

--- MasterMind.WPF/MainWindow.xaml.cs
+++ MasterMind.WPF/MainWindow.xaml.cs
@@ -9,11 +9,22 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Piotr Bacior - MasterMind Game - nr indeksu: 15 722 - WSEI Kraków
+
 namespace MasterMind.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
+    // Klasa reprezentująca pojedynczy element historii zgadywań w grze MasterMind
+    public class HistoryItem
+    {
+        public int AttemptNumber { get; set; }
+        public string GuessCode { get; set; }
+        public int Exact { get; set; }
+        public int Partial { get; set; }
+        public string ResultText { get; set; }
+    }
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
