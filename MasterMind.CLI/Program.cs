beecda189a2159a87fdaf696a97ba99faf65bf46
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-21 14:20:43 +0100

    com13 zad1 funkcja wyświetlania zgadywania przez użytkowników

--- MasterMind.CLI/Program.cs
+++ MasterMind.CLI/Program.cs
@@ -16,13 +16,13 @@ namespace MasterMind.CLI
             Console.WriteLine("Twoim zadaniem jest odgadnąć kod składający się z 4 kolorów");
             Console.WriteLine($"Dostępne kolory: {game.GetAllowedColors()}");
             Console.WriteLine("Oznaczenia wyniku: [X,Y] gdzie X to idealne trafienia (czarne), a Y to zła pozycja (białe).\n");
-            Console.WriteLine("---PB15722------PB15722------PB15722---");
+            Console.WriteLine("---PB15722------PB15722------PB15722---\n");
 
             // Główna pętla gry - kontynuujemy aż do zakończenia gry
             while (!game.isGameOver)
             {
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.WriteLine($"Próba {game.AttemptsUsed + 1}");
+                Console.WriteLine($"Próba {game.AttemptsUsed + 1}\n");
                 Console.ResetColor();
 
                 // Pobieramy dane wejściowe od użytkownika 
@@ -32,14 +32,51 @@ namespace MasterMind.CLI
                 if (string.IsNullOrEmpty(input) || input.Length != 4)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Nieprawidłowe dane wejściowe. Wprowadź dokładnie 4 litery reprezentujące kolory.");
+                    Console.WriteLine("Nieprawidłowe dane wejściowe. Wprowadź dokładnie 4 litery reprezentujące kolory.\n");
                     Console.ResetColor();   
                     continue;                // przechodzimy do następnej iteracji pętli
                 }
 
-               
+                // Funkcja do wyświetlania wyniku zgadywania 
+                try
+                {
+                    var result = game.EvaluateGuess(input);         // oceniamy zgadywanie użytkownika
 
+                    DisplayResult(result);                          // wyświetlamy wynik zgadywania
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Błąd: {ex.Message}\n");
+                }
             }
+        }
+
+        // Funkcja do wyświetlania wyniku zgadywania w czytelny sposób
+        static void DisplayResult(GuessResult result)
+        {
+            // Wyświetlamy wynik zgadywania za pomocą symboli i ustawiamy odpowiedni kolor tła konsoli
+            Console.WriteLine("Wynik:\n");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            // Wyświetlamy idealne trafienia i trafienia na złej pozycji
+            for (int i = 0; i < result.ExactMatches; i++)
+            {
+                Console.Write("* ");     // idealne trafienia
+            }
+
+            // ustawiamy kolor dla trafień na złej pozycji i wyświetlamy je 
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < result.PartialMatches; i++)
+            {
+                Console.Write("o ");     // trafienia na złej pozycji
+            }
+
+            // Resetujemy kolor konsoli do domyślnego
+            Console.ResetColor();
+
+            // Wyświetlamy dokładne liczby trafień i trafień na złej pozycji`
+            Console.WriteLine($" (Dokładne: {result.ExactMatches}, Zła pozycja: {result.PartialMatches})\n");
         }
     }
 }
