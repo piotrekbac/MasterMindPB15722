5ee1ccfcda519a6c0a197ab979af065f4cb4906c
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-22 17:57:57 +0100

    com9 zad2 klasa ComputerSolver - initial

---
+++ MasterMind.Engine/ComputerSolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Piotr Bacior - MasterMind Game Engine - nr indeksu: 15 722 - WSEI Kraków
+
+namespace MasterMind.Engine
+{
+    internal class ComputerSolver
+    {
+    }
+}
