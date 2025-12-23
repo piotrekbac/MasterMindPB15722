efabe6d618fb4865a9bd16a9d850f3b6b81f4b29
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-12-23 02:58:44 +0100

    com7 zad3 - pętla for dla sprawdzenia trafień niedokładnych (częściowych - partial)

--- MasterMind.Engine/Game.cs
+++ MasterMind.Engine/Game.cs
@@ -391,6 +391,26 @@ namespace MasterMind.Engine
             }
 
 
+            int partialMatches = 0;                             // Inicjalizujemy licznik trafień niedokładnych
+
+            // Szukamy tego koloru w sekretnym kodzie, pomijając już trafione dokładnie pozycje
+            for (int i = 0; i < len; i++)
+            {
+                if (guessMatched[i]) continue;                  // Pomijamy już trafione dokładnie pozycje
+
+                // Szukamy koloru z próby w kodzie sekretnym
+                for (int j = 0; j < len; j++)
+                {
+                    if (!secretMatched[j] && guess[i] == secret[j])
+                    {
+                        partialMatches++;                       // Zwiększamy licznik trafień niedokładnych
+                        secretMatched[j] = true;                // Zużywamy pozycję w kodzie sekretnym
+                        break;                                  // Przechodzimy do następnej pozycji w zgadywaniu
+                    }
+                }
+            }
+
+            return new GuessResult(exactMatches, partialMatches);       // Zwracamy wynik zgadywania
         }
     }
 }
